ce12ba28986232d009c2ee4deef065121834d011
MixolydianBY <lasy.anton@gmail.com>
2017-09-18 22:27:25 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyDLManagerQT',
+    version='0.0.1a',
+    packages=[find_packages()],
+    install_requires=['click', 'requests', 'progressbar2', 'pyperclip'],
+    url='https://github.com/MixolydianBY/PyDLManagerQT.git',
+    license='',
+    author='Mixolydian',
+    author_email='',
+    description=''
+)
